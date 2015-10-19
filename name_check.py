eb375154ad76d60b3cd7f8666905c1db3879e5a5
xiaoguaishou <sollnd@foxmail.com>
2015-10-19 17:19:32 +0800

    name_check_task

---
+++ name_check.py
@@ -0,0 +1,11 @@
+#edit by liuyun
+def sdscloud_container_name_check(container_name):
+    pass
+
+#edit by lijing
+def sdscloud_object_name_check(object_name):
+    pass
+
+if __name__ == "__main__":
+    pass
+
