5358d147624dd1bab7a4b5b571d73e13b7f64fdd
xiaoguaishou <sollnd@foxmail.com>
2015-10-23 21:04:33 +0800

    add container_name_check codes

--- name_check.py
+++ name_check.py
@@ -1,11 +1,24 @@
 #edit by liuyun
+import re
+
 def sdscloud_container_name_check(container_name):
-    pass
+#consist of small letter,digit and '-'
+    if not container_name[0].isdigit() and not container_name[0].isalpha():
+        return False
+    if not container_name[-1].isdigit() and not container_name[-1].isalpha():
+        return False
+    if len(container_name) < 3 or len(container_name) > 63:
+        return False
+    pattern = re.compile('[a-z0-9-]+')
+    m = pattern.match(container_name)
+    if not m or m.group() != container_name:
+        return False
+    return True
 
 #edit by lijing
 def sdscloud_object_name_check(object_name):
     pass
 
 if __name__ == "__main__":
-    pass
+    main()
 
