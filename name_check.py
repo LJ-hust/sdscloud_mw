816ad85d0340977d2c7a441155edcacdf17b576c
LJ-hust <604665748@qq.com>
2015-10-24 13:03:29 +0800

    add object_name_check by lijing

--- name_check.py
+++ name_check.py
@@ -1,5 +1,7 @@
 import re
 
+from libcloud.utils.py3 import urlquote
+from libcloud.storage.types import InvalidObjectNameError
 
 #edit by liuyun
 def sdscloud_container_name_check(container_name):
@@ -20,6 +22,21 @@ def sdscloud_container_name_check(container_name):
 
 #edit by lijing
 def sdscloud_object_name_check(object_name):
+    #the first_name can not be '/' or '\'
+    #'\n' and '\r' can not be in the name
+    #length can not more than 128 byte
+
+    if object_name[0] in ("/","\\"):
+        raise InvalidObjectNameError("the first character can not be '/' and '\'")
+
+    if "\r" in object_name or "\n" in object_name:
+        raise InvalidObjectNameError("the name can not have '\r' and '\n'")
+
+    object_name = urlquote(object_name)
+    if len(object_name) > 128:
+        raise InvalidObjectNameError(value='Object name length must less than 128',object_name=object_name)
+
+    return True
     pass
 
 if __name__ == "__main__":
