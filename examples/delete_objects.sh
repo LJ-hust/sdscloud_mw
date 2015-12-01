08d9cd96df6599e0eafcf0aa115493ea9833de4a
kun--hust <yichuan0707@163.com>
2015-12-01 16:28:22 +0800

    fix delete_objects

--- examples/delete_objects.sh
+++ examples/delete_objects.sh
@@ -1,14 +1,15 @@
 # !/bin/bash 
 
-con_name='lijing'
-Token='X-Auth-Token: AUTH_tk4baac626106e4c5f983eddb6720b6b0e'
+con_name='5ae98846cc9d48b2ba0d72c58c30369b_0'
+Token='AUTH_tk1174caebfc574073914c780a663ab0ee'
 
-rm var*
-curl -i -X GET -H 'X-Auth-Token: AUTH_tk4baac626106e4c5f983eddb6720b6b0e' http://127.0.0.1:8080/v1/AUTH_test/lijing >> var.txt
+curl -i -X GET -H 'X-Auth-Token: '${Token} http://127.0.0.1:8080/v1/AUTH_test/$con_name >> var.txt
 sed -n '12,$p' var.txt >> var1.txt
 chmod 777 var1.txt
 
 for line in `cat ./var1.txt`
 do
-  curl -X DELETE  -H 'X-Auth-Token: AUTH_tk4baac626106e4c5f983eddb6720b6b0e'  http://127.0.0.1:8080/v1/AUTH_test/$con_name/$line
+  curl -X DELETE  -H 'X-Auth-Token: '${Token}  http://127.0.0.1:8080/v1/AUTH_test/$con_name/$line
 done
+
+rm var*
